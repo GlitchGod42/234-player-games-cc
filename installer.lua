d48b0880a991f7bceba4e82db1aa4efaa2158207
GlitchGod42 <tobikmotyka54@gmail.com>
2026-05-06 14:42:08 +0200

    added mashing.nfp to installer

--- installer.lua
+++ installer.lua
@@ -10,9 +10,10 @@ shell.run("cd " .. path)
 term.clear()
 term.setCursorPos(1, 1)
 print("making directories")
-fs.makeDir(path .. "menus")
-fs.makeDir(path .. "minigames")
-fs.makeDir(path .. "modules")
+fs.makeDir(path .. "/menus")
+fs.makeDir(path .. "/minigames")
+fs.makeDir(path .. "/modules")
+fs.makeDir(path .. "/nfps")
 print("installing scripts")
 shell.run("wget https://raw.githubusercontent.com/GlitchGod42/234-player-games-cc/refs/heads/main/234-player-games/main.lua")
 shell.run("cd menus")
@@ -23,6 +24,9 @@ shell.run("wget https://raw.githubusercontent.com/GlitchGod42/234-player-games-c
 
 shell.run("cd ../minigames")
 shell.run("wget https://raw.githubusercontent.com/GlitchGod42/234-player-games-cc/refs/heads/main/234-player-games/minigames/mashing.lua")
+
+shell.run("cd ../nfps")
+shell.run("wget https://raw.githubusercontent.com/GlitchGod42/234-player-games-cc/refs/heads/main/234-player-games/nfps/mashing.nfp")
 
 print("Done!")
 print("Hit enter to exit")
